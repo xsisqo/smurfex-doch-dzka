a2dead1451c0ccbbfae4115b2a70d4688da51885
xsisqo <96359214+xsisqo@users.noreply.github.com>
2026-07-02 01:29:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,7 @@
-const CACHE_NAME = "smurfex-dochadzka-v2-sk-en";
-const FILES = ["./","./index.html","./style.css","./app.js","./manifest.json","./logo.png","./icon-192.png","./icon-512.png"];
-self.addEventListener("install", event => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))));
-});
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
